e1032c170378b9f1644ccaf20f50c291073d4597
Emmanuel Dedryver <marcossuarezr88@gmail.com>
2023-05-17 20:08:21 +0200

    cypress

--- src/pages/GalerieMyGalerie.cy.js
+++ src/pages/GalerieMyGalerie.cy.js
@@ -1,11 +1,11 @@
 import { mount } from '@cypress/react';
 import '@cypress/react/support';
+import Galerie from '../components/Galerie/Galerie';
 
 Cypress.Commands.add('mount', mount);
 
 describe('<MyGalerie />', () => {
   it('renders', () => {
-    cy.mount(<MyGalerie />);
-
-  })
-})
+    cy.mount(<Galerie />);
+  });
+});
