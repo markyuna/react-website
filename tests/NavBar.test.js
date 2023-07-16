2e9a6d0691ae370fa206dd8faf8131042b8d9717
MARCOS <marcossuarezr88@gmail.com>
2023-07-16 22:41:08 +0200

    NavBar test updated

--- tests/NavBar.test.js
+++ tests/NavBar.test.js
@@ -1,20 +1,11 @@
-// import { render, screen } from '@testing-library/react';
-// import Navbar from '../src/components/Navbar/Navbar';
-// import '@testing-library/jest-dom'
 
-// describe('Navbar component', () => {
-//     test('The Logo is in the page', () => {
-//        render(<Navbar />)
-//        const article = screen.getByRole('logo', { name: 'logo' })
-//        expect(article).toBeInTheDocument()
-//     });
+import { test, expect } from '@playwright/test'
+import LoginButton from '../src/components/Form/Login'
+import { render } from '@testing-library/react';
 
-//     test('la caja de texto se encuentra en el documento ', () => {
-//         render(<Navbar />)
-//         const article = screen.getByRole('article', { name: 'parrafo principal' })
-//         expect(article).toHaveStyle({
-//             backgroundColor: 'indigo'
-//         })
-//     }); 
-    
-// })
+test('Login', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    render(<LoginButton />)
+    await expect(page).toHaveURL();
+    await page.getByRole('link', { name: 'Conexion' }).click();
+});
--- tests/playwright.test.ts
+++ tests/playwright.test.ts
@@ -8,3 +8,16 @@ test('Mon premier test', async ({page}) => {
   await getStarted.click()
   await expect(page.locator('text=Sculptures').first()).toBeVisible()
 })
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('link', { name: 'Login' }).click();
+  await expect(page).toHaveTitle(/React App/)
+  await page.getByText('Conexion').click();
+  await page.getByRole('button', { name: 'Continue with Google' }).click();
+  await page.getByLabel('Adresse e-mail ou numéro de téléphone').click();
+  await page.getByRole('button', { name: 'Suivant' }).click();
+  await page.getByLabel('Réessayer').click();
+  await page.getByLabel('Adresse e-mail ou numéro de téléphone').fill('');
+  await page.getByRole('button', { name: 'Suivant' }).click();
+});
